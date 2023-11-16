2476c402d41f8f8706198fa2ecb6396282f0f828
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-16 14:04:47 +0900

    feat: 날짜 가로로 넘기기
    - ScrollView 컴포넌트 : 페이지 스크롤 기능
    - horizontal 속성 : 가로로 스크롤 가능
    - pagingEnabled : 페이지 단위로 스크롤을 넘길  수 있음
    - showHoriziontalScrollIndicator : 스크롤 바 노출 x

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Dimensions, Text, ScrollView } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
@@ -6,12 +8,32 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false} style={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -30,15 +52,14 @@ const styles = StyleSheet.create({
 
   cityName: {
     fontSize: 68,
-    fontWeight: '500'
+    fontWeight: '500',
   },
 
   weather: {
-    flex: 3,
   },
 
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: 'center',
   },
 
@@ -51,4 +72,4 @@ const styles = StyleSheet.create({
     fontSize: 60,
     marginTop: -30,
   },
-})
+});
