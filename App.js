209a52608870e8a1e928c38add7c3b33ddece7b0
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-16 16:30:35 +0900

    feat: icon 삽입

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { WEATHER_API_KEY } from '@env';
 import { useEffect, useState } from 'react';
 import { View, StyleSheet, Dimensions, Text, ScrollView, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
+import { Fontisto } from '@expo/vector-icons';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
@@ -9,6 +10,16 @@ export default function App() {
   const [city, setCity] = useState('Loading...');
   const [days, setDays] = useState([]);
   const [permission, setPermission] = useState(true);
+
+  const icons = {
+    Clouds: 'cloudy',
+    Clear: 'day-sunny',
+    Atmosphere: 'cloudy-gusts',
+    Snow: 'snow',
+    Rain: 'rains',
+    Drizzle: 'rain',
+    Thunderstorm: 'lightning',
+  };
 
   const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
@@ -46,13 +57,16 @@ export default function App() {
       </View>
       <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false} style={styles.weather}>
         {days.length === 0 ? (
-          <View style={styles.day}>
+          <View style={{ ...styles.day, alignItems: 'center' }}>
             <ActivityIndicator color='black' size='large' />
           </View>
         ) : (
           days.map((day, idx) => (
             <View key={idx} style={styles.day}>
-              <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+              <View style={styles.temp_icon}>
+                <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+                <Fontisto name={icons[day.weather[0].main]} size={60} color='black' />
+              </View>
               <Text style={styles.main}>{day.weather[0].main}</Text>
               <Text style={styles.description}>{day.weather[0].description}</Text>
             </View>
@@ -84,19 +98,29 @@ const styles = StyleSheet.create({
 
   day: {
     width: SCREEN_WIDTH,
-    alignItems: 'center',
+    alignItems: 'left',
+    padding: 10,
   },
 
   temp: {
-    marginTop: 50,
-    fontSize: 178,
+    marginTop: 30,
+    fontSize: 120,
+    fontWeight: '500',
   },
 
   main: {
-    fontSize: 60,
-    marginTop: -30,
+    fontSize: 40,
+    marginTop: -10,
   },
+
   description: {
-    fontSize: 20,
-  }
+    fontSize: 30,
+  },
+
+  temp_icon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
 });
